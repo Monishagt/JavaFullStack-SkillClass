b9dfc0f34d8ce18d5d57c91ac39c60cbe9dec218
Monishagt <158596098+Monishagt@users.noreply.github.com>
2024-06-15 11:33:43 +0530

    Add files via upload

---
+++ MyLab1/src/main/java/com/pes/MyLab1/MyLab1Application.java
@@ -0,0 +1,13 @@
+package com.pes.MyLab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyLab1Application.class, args);
+	}
+
+}
---
+++ MyLab1/src/test/java/com/pes/MyLab1/MyLab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pes.MyLab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
